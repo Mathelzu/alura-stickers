d001cf81bda055152b84ebbcc89cf50bb3a72df7
Matheus Felipe <matheusprogramacao2003@gmail.com>
2022-07-18 19:49:48 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão  HTTP e buscar os top 250 filmes
+        String url ="https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        // pegar só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String , String>> listaDeFilmes = parser.parse(body);
+        System.out.println(listaDeFilmes.size());
+        System.out.println(listaDeFilmes.get(0));
+
+        // exibir e manipular os dados
+
+        for (Map<String , String> filme : listaDeFilmes){
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+
+        }
+    }
+}
